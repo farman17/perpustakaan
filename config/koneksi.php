8857e52a48fedf5b74d8ed08a3299853f923171a
farman17 <ahmadfarman1701@gmail.com>
2020-12-24 11:18:02 +0000

    melakukan perubahan pada Dockerfile..

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
 // mysqli_connect("hostname","user database","password","nama")
-$koneksi= mysqli_connect("localhost","root","","dbperpus") or die("Tidak bisa terhubungan ke database");
+$koneksi= mysqli_connect("dbsosmedia.cjorhe5jzkni.us-east-2.rds.amazonaws.com","admin","farman17","dbperpus") or die("Tidak bisa terhubungan ke database");
 
 
